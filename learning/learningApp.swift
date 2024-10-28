7c14e469e2844b121d3dbc1773ef2a2d681d626c
NoufBinHassan <noufapplebh@gmail.com>
2024-10-28 19:28:29 +0300

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Nouf Bin Hassan on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learningTests/learningTests.swift
@@ -0,0 +1,17 @@
+//
+//  learningTests.swift
+//  learningTests
+//
+//  Created by Nouf Bin Hassan on 28/10/2024.
+//
+
+import Testing
+@testable import learning
+
+struct learningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
